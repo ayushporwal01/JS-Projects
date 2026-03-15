de1d8ac708e69fb1784730a65f536b29a47a1052
ayushporwal01 <porwalayush953@gmail.com>
2026-03-15 09:52:17 +0530

    Initial commit

--- Todo List/script.js
+++ Todo List/script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const taskInput = document.getElementById('taskInput')
-    const addTaskBtn = document.getElementById('addTaskBtn')
-    const editBtn = document.getElementById('editBtn')
-    const deleteBtn = document.getElementById('deleteBtn')
+    const addTaskBtn = document.getElementById('add-task-btn')
+    const editBtn = document.getElementById('edit-btn')
+    const deleteBtn = document.getElementById('delete-btn')
     const taskList = document.getElementById('taskList')
 
     addTaskBtn.addEventListener("click", () => {
@@ -21,8 +21,8 @@ document.addEventListener('DOMContentLoaded', () => {
         li.innerHTML = 
         `<span>{${taskText}</span>
         <div>
-           <button id="editBtn">Edit</button>
-           <button id="deleteBtn">Delete</button>
+           <button id="edit-btn">Edit</button>
+           <button id="delete-btn">Delete</button>
         </div>
         `
     }
