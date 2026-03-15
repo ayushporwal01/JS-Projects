ccfd2a01faf1364f154eb291cb6539c141261d1a
ayushporwal01 <porwalayush953@gmail.com>
2026-03-15 09:44:48 +0530

    Initial commit

--- Todo List/script.js
+++ Todo List/script.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const taskInput = document.getElementById('taskInput')
+    const addBtn = document.getElementById('addBtn')
+    const editBtn = document.getElementById('editBtn')
+    const deleteBtn = document.getElementById('deleteBtn')
     const taskList = document.getElementById('taskList')
 
 
