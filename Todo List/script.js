a5571426f22a0ee3dfd860ff453b28ddb880b94e
ayushporwal01 <porwalayush953@gmail.com>
2026-03-15 10:37:09 +0530

    Initial commit

--- Todo List/script.js
+++ Todo List/script.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
     //Add Task On Enter Key
     taskInput.addEventListener('keydown', (e) => {
         if(e.key === "Enter") {
-           addTask(taskText)
+           addTask.click()
         }
     })
 
