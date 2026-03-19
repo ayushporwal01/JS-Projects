6dd028af10b5a1221599bd1ac828f4230fe2945e
ayushporwal01 <porwalayush953@gmail.com>
2026-03-19 17:25:46 +0530

    Initial commit

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,5 +1,6 @@
-const apiKey = "50965f50b67b56afdf33799237c592a6";
 
 async function getWeather() {
-    const city = document.getElementById('')
+    const city = document.getElementById('city');
+    const apiKey = "50965f50b67b56afdf33799237c592a6";
+
 }
