3c6289f5632cd1b24f7ee32ef280eef9c96c2e39
ayushporwal01 <porwalayush953@gmail.com>
2026-03-19 17:26:04 +0530

    Initial commit

--- Weather App/script.js
+++ Weather App/script.js
@@ -2,5 +2,6 @@
 async function getWeather() {
     const city = document.getElementById('city');
     const apiKey = "50965f50b67b56afdf33799237c592a6";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 }
