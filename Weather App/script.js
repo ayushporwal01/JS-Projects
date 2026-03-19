f7291b602c4c38f3ed889da2def2a2559f68e5a5
ayushporwal01 <porwalayush953@gmail.com>
2026-03-19 17:41:24 +0530

    Initial commit

--- Weather App/script.js
+++ Weather App/script.js
@@ -20,7 +20,7 @@ async function getWeather() {
         document.getElementById("result").innerHTML = `
           <h3>${data.name}</h3>
           <p>Temperature: ${temp}°C</p>
-          <p>Weather: ${weather}°C</p>
+          <p>Weather: ${weather}</p>
           <p>Humidity: ${humidity}%</p>
         `;
     }
