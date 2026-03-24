aebe2862301a55dc6bb65f70b855a47e1466991c
ayushporwal01 <porwalayush953@gmail.com>
2026-03-24 11:01:25 +0530

    Initial commit

--- Weather App/script.js
+++ Weather App/script.js
@@ -30,6 +30,7 @@ async function getWeather() {
         cityNameEl.textContent = data.name;
         tempEl.textContent = `Temperature: ${data.main.temp} °C`;
         weatherEl.textContent = `Weather: ${data.weather[0].description}`;
+        precipitationEl.textContent = `Precipitation: ${data.precipitation[0].description}`;
         humidityEl.textContent = `Humidity: ${data.main.humidity}%`;
     }
 
